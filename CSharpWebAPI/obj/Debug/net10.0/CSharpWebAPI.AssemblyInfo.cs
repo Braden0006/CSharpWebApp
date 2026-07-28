5b7e9b1e96548a909172df2d5c5fc43cfed3b595
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2026-07-28 14:23:32 -0600

    Removed weather forecast boiler plate code in 'Program.cs'

--- CSharpWebAPI/obj/Debug/net10.0/CSharpWebAPI.AssemblyInfo.cs
+++ CSharpWebAPI/obj/Debug/net10.0/CSharpWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharpWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+454b5eda6063465d0adb88bf5c418303d100eb8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4ec0013267c4f34263d86c13d63266e21327edf")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharpWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharpWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
