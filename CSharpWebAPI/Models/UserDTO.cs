135d7c0f192ede73f44199dcf9126daba04b26ab
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2026-08-06 08:58:06 -0600

    Added a Data Transfer Object for the 'User' model class

--- CSharpWebAPI/Models/UserDTO.cs
+++ CSharpWebAPI/Models/UserDTO.cs
@@ -1,11 +1,11 @@
 namespace CSharpWebAPI.Models;
 
-public class User
+public class UserDTO
 {
     public long Id { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? Email { get; set; }
-    public string? UserName { get; set; }
+    public string? Username { get; set; }
     public string? Password { get; set; }
 }
