de55e2981528cda8b6139d8c055b638215de36da
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2026-08-09 22:07:53 -0600

    Added everything in 'Program.cs' for Blazor pages functionality

--- CSharpWebAPI/Program.cs
+++ CSharpWebAPI/Program.cs
@@ -6,7 +6,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Adds services for Razor pages / MVC
 builder.Services.AddControllersWithViews();
+
+// This is for Blazor server interactivity
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
 // Adds the database context to the DI container and specifies that it will use 
 // an in-memory database. 
@@ -21,9 +26,18 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+// Required for interactive components since .NET 8
+app.UseAntiforgery();
+
 app.UseHttpsRedirection();
 
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
 app.MapControllers();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapGet("/", () => "Hello World!");
 
